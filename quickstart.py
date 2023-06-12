de3d77e4963dcda81cd6afc2dd2a9fd582d57688
ansalr <ansalr.email@gmail.com>
2023-06-12 15:44:20 +0530

    final commit

--- quickstart.py
+++ quickstart.py
@@ -1,8 +1,18 @@
-# pip install OpenAI
 # pip install langchain
+# pip install OpenAI
 
 
-import os
-os.environ["OPENAI_API_KEY"] = "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"
+import os 
+os.environ["OPENAI_API_KEY"]= "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"
 
-from langchain import OpenAI
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+
+
+llm = OpenAI(temperature=0.9)
+
+
+prompt = PromptTemplate(input_variables=['food'],
+                        template="top 5 destination in india to have {food}?")
+
+print(llm(prompt.format(food="coffe")))
